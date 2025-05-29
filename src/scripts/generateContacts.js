c16ccd04724ae7093442d20442983d5d0dbb5119
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:04:52 +0300

    STEP 3 DONE

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-generateContacts(5);
+export const generateContacts = (count) => {
+  const contacts = readContacts();
+  for (let i = 0; i < count; i++) {
+    contacts.push(createFakeContact());
+  }
+  writeContacts(contacts);
+};
