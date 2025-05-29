3b2be53e198c71cecfaf227194b258e2b75d8a80
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:31:22 +0300

    addOneContact.js fixed with try catch

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -3,7 +3,13 @@ import { writeContacts } from '../utils/writeContacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const addOneContact = () => {
-  const contacts = readContacts();
-  contacts.push(createFakeContact());
-  writeContacts(contacts);
+  try {
+    const contacts = readContacts();
+    contacts.push(createFakeContact());
+    writeContacts(contacts);
+  } catch (error) {
+    console.error('Error adding one contact:', error.message);
+  }
 };
+
+addOneContact();
