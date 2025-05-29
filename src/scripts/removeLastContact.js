dadf444a3f310c13c4868149d897f1d5f164e120
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:38:35 +0300

    fixed removeLast

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,11 +1,18 @@
-import { readContacts } from '../utils/readContacts.js';
+import { getAllContacts } from './getAllContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
-export const removeLastContact = () => {
-  const contacts = readContacts();
-  if (contacts.length > 0) {
-    contacts.pop();
-    writeContacts(contacts);
+export const removeLastContact = async () => {
+  try {
+    const contacts = await getAllContacts();
+    if (contacts.length === 0) {
+      console.log('No contacts to remove.');
+      return;
+    }
+    const updatedContacts = contacts.slice(0, -1);
+    await writeContacts(updatedContacts);
+    console.log('Last contact removed successfully.');
+  } catch (err) {
+    console.error('Error:', err);
   }
 };
 
