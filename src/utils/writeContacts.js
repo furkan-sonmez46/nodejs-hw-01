8cbe18a26ac5bf538b259a8c795306e144ab0f1c
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:00:54 +0300

    STEP 2 DONE (WRITING)

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
+import path from 'path';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = (data) => {
+  fs.writeFileSync(
+    path.resolve(PATH_DB),
+    JSON.stringify(data, null, 2),
+    'utf-8',
+  );
+};
