a360243732de0a53aba6183ce730f3044a938f75
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:10:36 +0300

    STEP 6 DONE

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = () => {
+  const contacts = readContacts();
+  return contacts.length;
+};
 
 console.log(await countContacts());
