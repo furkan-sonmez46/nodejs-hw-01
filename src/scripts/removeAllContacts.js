ece5130801ea0f9d287b120f7ecd0aba02338620
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:37:43 +0300

    removeAll fixed

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,7 +1,13 @@
-import { writeContacts } from '../utils/writeContacts.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = () => {
-  writeContacts([]);
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, [], 'utf-8');
+    console.log('All contacts have been deleted successfully.');
+  } catch (err) {
+    console.error('Error writing to file:', err);
+  }
 };
 
 removeAllContacts();
