24b5d5b0bea8af54dfb5fcecf27be888dd88b998
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-30 01:13:33 +0300

    STEP 8 DONE

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,12 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = () => {
+  const contacts = readContacts();
+  if (contacts.length > 0) {
+    contacts.pop();
+    writeContacts(contacts);
+  }
+};
 
 removeLastContact();
