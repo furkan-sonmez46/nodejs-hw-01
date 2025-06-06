2d846f2b7954abb1396c70f5c788c08edb64b12e
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-06-06 16:21:00 +0300

    writeContent has fixed

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,10 +1,10 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 
 import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {
   try {
-    const data = JSON.stringify(updatedContacts);
+    const data = JSON.stringify(updatedContacts, null, 2);
     await fs.writeFile(PATH_DB, data, 'utf8');
     console.log('Data successfully written to file.');
   } catch (err) {
